487b6b60d8db0deb3c1ac7285d6c35935255548e
MOCHAMMAD RIZKI ROMDONI <cv.yacanet@gmail.com>
2023-01-23 00:12:16 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,18 @@
 apt update && apt upgrade -y && apt dist-upgrade -y
 
 apt install python3-venv git python3-dev libffi-dev gcc libssl-dev python3-docker -y
+
+python3 -m venv /path/to/venv
+source /path/to/venv/bin/activate
+
+pip install -U pip
+
+pip install 'ansible>=4,<6'
+
+pip install git+https://opendev.org/openstack/kolla-ansible@stable/zed
+
+mkdir -p /etc/kolla
+
+cp -r  /path/to/venv/share/kolla-ansible/etc_examples/kolla/* /etc/kolla
+cp /path/to/venv/share/kolla-ansible/ansible/inventory/all-in-one /etc/kolla
+
