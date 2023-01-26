a46bd9895d47fb820afbee33b8e55c4040b8cd73
MOCHAMMAD RIZKI ROMDONI <cv.yacanet@gmail.com>
2023-01-26 22:38:54 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,25 +7,25 @@ apt install python3-venv git python3-dev libffi-dev gcc libssl-dev python3-docke
 python3 -m venv /path/to/venv
 source /path/to/venv/bin/activate
 
-pip install -U pip
+source /path/to/venv/bin/activate && pip install -U pip
 
-pip install 'ansible>=4,<6'
+source /path/to/venv/bin/activate &&  pip install 'ansible>=4,<6'
 
-pip install git+https://opendev.org/openstack/kolla-ansible@stable/zed
+source /path/to/venv/bin/activate &&  pip install git+https://opendev.org/openstack/kolla-ansible@stable/zed
 
 mkdir -p /etc/kolla
 
 cp -r  /path/to/venv/share/kolla-ansible/etc_examples/kolla/* /etc/kolla
 cp /path/to/venv/share/kolla-ansible/ansible/inventory/all-in-one /etc/kolla
 
-kolla-ansible install-deps
+source /path/to/venv/bin/activate && kolla-ansible install-deps
 
 source /path/to/venv/bin/activate && cd /etc/kolla && kolla-genpwd
 
-cd /home/customer/gurindam-cloud
+cd /home/customer/gurindam-cloud && cp -R globals.yml /etc/kolla
 
 cd /home/customer/gurindam-cloud && kolla-ansible -i ./all-in-one bootstrap-servers
 cd /home/customer/gurindam-cloud && kolla-ansible -i ./all-in-one prechecks
 cd /home/customer/gurindam-cloud && kolla-ansible -i ./all-in-one deploy
-pip install python-openstackclient
-kolla-ansible post-deploy
+source /path/to/venv/bin/activate && pip install python-openstackclient
+source /path/to/venv/bin/activate && kolla-ansible post-deploy
